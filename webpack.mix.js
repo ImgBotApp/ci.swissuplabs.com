4fd0d3fe9d92325481a0a47735bbc000a1e832be
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2017-08-15 21:48:55 +0300

    Kube and Basscss powered frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,15 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+    .options({
+        postCss: [
+            require('cssnano')({
+                preset: ['default', {
+                    discardComments: {
+                        removeAll: true
+                    }
+                }]
+            })
+        ]
+   });
