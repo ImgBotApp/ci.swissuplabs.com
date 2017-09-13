dc59b1b70c6d037198f1795825ac0b3a2132bec8
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2017-09-13 22:48:05 +0300

    Run MEQP test

--- app/Jobs/ValidateGithubCommit.php
+++ app/Jobs/ValidateGithubCommit.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App;
 use Activity;
 
 use Illuminate\Bus\Queueable;
@@ -37,6 +38,22 @@ class ValidateGithubCommit implements ShouldQueue
      */
     public function handle()
     {
+        try {
+            $testErrors = $this->downloadSources()->runTests();
+            // update commit status at github.com
+        } catch (\Exception $e) {
+            Activity::log('ValidateGithubCommit: Failure. ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Download module sources into 'storage/app' folder
+     *
+     * @return $this
+     * @throws \Exception
+     */
+    private function downloadSources()
+    {
         $repository = array_get($this->payload, 'repository.full_name');
         $cloneUrl   = array_get($this->payload, 'repository.clone_url');
         $cloneUrl   = str_replace(
@@ -50,8 +67,9 @@ class ValidateGithubCommit implements ShouldQueue
         // @todo: replace this code with archive download
         // in case of slow git fetch command
         // @see https://developer.github.com/v3/repos/contents/#get-archive-link
+        $folder = escapeshellarg(storage_path('app/' . $repository));
         $command = implode(' && ', [
-            'cd ' . escapeshellarg(storage_path('app/' . $repository)),
+            'cd ' . $folder,
             'git init',
             'git fetch ' . escapeshellarg($cloneUrl),
             'git checkout ' . escapeshellarg(array_get($this->payload, 'head_commit.id')),
@@ -61,15 +79,54 @@ class ValidateGithubCommit implements ShouldQueue
         $process->run();
 
         if (!$process->isSuccessful()) {
-            Activity::log(sprintf(
-                'ValidateGithubCommit: Failure. Input: %s; Output: %s',
+            throw new \Exception(sprintf(
+                'Input: %s; Output: %s',
                 $command,
                 $process->getErrorOutput()
             ));
-        } else {
-            // run tests
         }
 
-        // update commit status at github.com
+        return $this;
+    }
+
+    /**
+     * Run tests against downloaded sources
+     *
+     * @return string       Error messages
+     * @throws \Exception
+     */
+    private function runTests()
+    {
+        $repository = array_get($this->payload, 'repository.full_name');
+        $folder = escapeshellarg(storage_path('app/' . $repository));
+
+        $command = implode(' && ', [
+            sprintf(
+                "%s/vendor/bin/phpcs --config-set installed_paths %s > /dev/null",
+                App::basePath(),
+                App::basePath() . '/vendor/magento/marketplace-eqp'
+            ),
+            sprintf(
+                "%s/vendor/bin/phpcs %s --standard=%s --severity=8",
+                App::basePath(),
+                $folder,
+                App::basePath() . '/vendor/magento/marketplace-eqp/MEQP2'
+            )
+        ]);
+
+        $process = new Process($command);
+        $process->run();
+
+        // phpcs uses exit(1) if validation errors where found, so in order
+        // to detect if it was really a terminal error - checkout error_output too
+        if (!$process->isSuccessful() && $process->getErrorOutput()) {
+            throw new \Exception(sprintf(
+                'Input: %s; Output: %s',
+                $command,
+                $process->getErrorOutput()
+            ));
+        }
+
+        return $process->getOutput();
     }
 }
