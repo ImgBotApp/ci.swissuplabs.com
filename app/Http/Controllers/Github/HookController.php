894d4868e5975e96483873b1b9d5556136db215d
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2017-11-20 22:09:50 +0200

    Unused classes removed

--- app/Http/Controllers/Github/HookController.php
+++ app/Http/Controllers/Github/HookController.php
@@ -4,11 +4,7 @@ namespace App\Http\Controllers\Github;
 
 use App\Events\PushRecieved;
 use App\Push;
-use App\CommitRepository;
 use Illuminate\Http\Request;
-use App\Jobs\DebouncedJob;
-use App\Jobs\ValidateGithubCommit;
-use App\Jobs\UpdateComposerPackages;
 use App\Http\Controllers\Controller;
 
 class HookController extends Controller
